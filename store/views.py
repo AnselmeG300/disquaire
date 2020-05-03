1e53d1bb88bede20ee86f7490bd02f510c8e6f34
AnselmeG300IT <tchassem.anselme@stormit.fr>
2020-05-03 13:27:11 +0100

    [Affichez une liste de résultats sur plusieurs pages]:La pagination dans les vues

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Album, Artist, Contact, Booking
 
@@ -11,7 +12,21 @@ def index(request):
     return render(request, 'store/index.html', context)
 
 def listing(request):
-    albums = Album.objects.filter(available=True)
+    albums_list = Album.objects.filter(available=True)
+    # Slice pages
+    paginator = Paginator(albums_list, 6)
+    # Get current page number
+    page = request.GET.get('page')
+
+    try:
+        # Return only this page albums and not others
+        albums = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        albums = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        albums = paginator.page(paginator.num_pages)
     context = {
         'albums': albums
     }
