de385fbce42acfa2fbf9dd9275f86d2c4851ca85
AnselmeG300IT <tchassem.anselme@stormit.fr>
2020-05-03 13:00:24 +0100

    [Configurez un champ de recherche et une page 404]:Ajouter un champ de recherche

--- disquaire_project/settings.py
+++ disquaire_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '943gxfhvd7)j7p7#p=n0z056)kyzz*s!uf-v09)*pku%g52k$h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
