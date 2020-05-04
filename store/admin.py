187a8618ebfefb7276f3a4be6431c912dfaa49ac
AnselmeG300IT <tchassem.anselme@stormit.fr>
2020-05-04 13:39:05 +0100

    [Déterminez les informations pertinentes à afficher]: Ajout des informations provenant d'une autre table

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 
-from .models import Booking
+from .models import Booking, Contact, Album, Artist
 # Register your models here.
 
 admin.site.register(Booking)
+
+
+
+
+
+class AlbumArtistInline(admin.TabularInline):
+    model = Album.artists.through # the query goes through an intermediate table.
+    extra = 1
+class BookingInline(admin.TabularInline):
+    model = Booking
+    fieldsets = [
+        (None, {'fields': ['album', 'contacted']})
+        ] # list columns
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    inlines = [BookingInline,] # list of bookings made by a contact
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    inlines = [AlbumArtistInline,]
