b2994bc266357dfd8a8ad15f08e60e26d49eeac2
AnselmeG300IT <tchassem.anselme@stormit.fr>
2020-05-07 11:06:01 +0100

    [Testez votre application]: Tests utilisant des objets dans la base de données:Ajout de données dans la base de données

--- store/tests.py
+++ store/tests.py
@@ -6,3 +6,16 @@ class IndexPageTestCase(TestCase):
     def test_index_page(self):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
+
+from .models import Album, Artist, Contact, Booking
+
+class DetailPageTestCase(TestCase):
+
+    # test that detail page returns a 200 if the item exists.
+    def test_detail_page_returns_200(self):
+        impossible = Album.objects.create(title="Transmission Impossible")
+        album_id = Album.objects.get(title='Transmission Impossible').id
+        response = self.client.get(reverse('store:detail', args=(album_id,)))
+        self.assertEqual(response.status_code, 200)
+
+    # test that detail page returns a 404 if the item does not exist.
