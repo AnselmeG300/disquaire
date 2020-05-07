68e22645a5fd281aeaa245889dd0ec0e5b00dc60
AnselmeG300IT <tchassem.anselme@stormit.fr>
2020-05-07 11:08:45 +0100

    [Testez votre application]: Tests utilisant des objets dans la base de données: utilisez la methode setUp()

--- store/tests.py
+++ store/tests.py
@@ -11,16 +11,19 @@ from .models import Album, Artist, Contact, Booking
 
 class DetailPageTestCase(TestCase):
 
-    # test that detail page returns a 200 if the item exists.
-    def test_detail_page_returns_200(self):
+    # ran before each test.
+    def setUp(self):
         impossible = Album.objects.create(title="Transmission Impossible")
-        album_id = Album.objects.get(title='Transmission Impossible').id
+        self.album = Album.objects.get(title='Transmission Impossible')
+
+    # test that detail page returns a 200 if the item exists
+    def test_detail_page_returns_200(self):
+        album_id = self.album.id
         response = self.client.get(reverse('store:detail', args=(album_id,)))
         self.assertEqual(response.status_code, 200)
 
     # test that detail page returns a 404 if the item does not exist
     def test_detail_page_returns_404(self):
-        impossible = Album.objects.create(title="Transmission Impossible")
-        album_id = Album.objects.get(title='Transmission Impossible').id + 1
+        album_id = self.album.id + 1
         response = self.client.get(reverse('store:detail', args=(album_id,)))
         self.assertEqual(response.status_code, 404)
