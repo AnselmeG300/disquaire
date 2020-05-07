ce34e3d758814177ce1fc4393fd5e1d47cdef00a
AnselmeG300IT <tchassem.anselme@stormit.fr>
2020-05-07 11:01:57 +0100

    [Testez votre application]: Test du code de statut renvoyé par une vue

--- store/tests.py
+++ store/tests.py
@@ -1,14 +1,8 @@
+from django.urls import reverse
 from django.test import TestCase
 
-# Index page
-    # test that index page returns a 200
-
-# Detail Page
-    # test that detail page returns a 200 if the item exists
-    # test that detail page returns a 404 if the item does not exist
-
-# Booking Page
-    # test that a new booking is made
-    # test that a booking belongs to a contact
-    # test that a booking belongs to an album
-    # test that an album is not available after a booking is made
+# Create your tests here.
+class IndexPageTestCase(TestCase):
+    def test_index_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
